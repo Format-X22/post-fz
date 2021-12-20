5c4e7f943e7aa90fa255f968fafe16993189c36b
oleg <opa@exante.eu>
2021-12-20 13:41:04 +0300

    Bootstrap.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { QuestModule } from './quest/quest.module';
+import { SettingsModule } from './settings/settings.module';
+import { ViewModule } from './view/view.module';
+import { ChatModule } from './chat/chat.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ThrottlerModule.forRoot({
+            ttl: 60,
+            limit: 10,
+        }),
+        AuthModule,
+        QuestModule,
+        SettingsModule,
+        ViewModule,
+        ChatModule,
+    ],
+    controllers: [AppController],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+        AppService,
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.use(cookieParser());
+    app.use(helmet());
+
+    await app.listen(3003);
 }
 bootstrap();
